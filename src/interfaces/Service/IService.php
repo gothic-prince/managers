7cd48c934e4c1d28eb601b04606b6ec19b11cd6b
Pavel Gafarov <gothic27prince@gmail.com>
2016-06-27 22:12:50 +0400

    Service

--- src/interfaces/Service/IService.php
+++ src/interfaces/Service/IService.php
@@ -1,9 +1,11 @@
 <?php
 namespace cmspp\managers\interfaces\Service;
+use cmspp\events\interfaces\dependencies\IDependencies;
 use cmspp\events\interfaces\info\IEventInfo;
 
 interface IService
 {
     public function getInfo(): IEventInfo;
     public function init();
+    public function getDependencies(): IDependencies;
 }
